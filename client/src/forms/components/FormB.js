7ce1f1a6ebe90890dcfe96d7b86c23ff977ccabe
kakarot_6909 <rahul.naravadkar@gmail.com>
2021-04-02 19:16:26 +0530

    Add. Adds deleteHandler to the MaterialTable component

--- client/src/forms/components/FormB.js
+++ client/src/forms/components/FormB.js
@@ -8,47 +8,47 @@ export default function FormB(){
     const [storeMaterial, setStoreMaterial] = useState([]);
     const [orderedMaterial, setOrderedMaterial] = useState([]);
 
-    useEffect(() => {
+    // useEffect(() => {}, [storeMaterial,orderedMaterial])
 
-    }, [storeMaterial,orderedMaterial])
-
-    const availableHandler = (material, price) => {
-        setStoreMaterial([...storeMaterial,{material,price}]);
+    const availableHandler = (material, approxCost) => {
+        setStoreMaterial([...storeMaterial,{material,approxCost}]);
     }
 
-    const orderedHandler = (material, price) => {
-        setOrderedMaterial([...orderedMaterial,{material,price}]);
+    const orderedHandler = (material, approxCost) => {
+        setOrderedMaterial([...orderedMaterial,{material,approxCost}]);
     }
 
     return(
         <Grid container spacing={4}>
             <Grid item lg={6} md={12}>
-                <Grid>
-                    <Typography variant='h4'>
-                        Available in Store
-                    </Typography>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <Typography variant='h4'>
+                            Available in Store
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <MaterialForm type = {'available'} submitHandler = {availableHandler} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <MaterialTable data={storeMaterial} setData = {setStoreMaterial} />
+                    </Grid>
                 </Grid>
-                <Grid>
-                    <MaterialForm type = {'available'} submitHandler = {availableHandler} />
-                </Grid>
-                <Grid>
-                    <MaterialTable data={storeMaterial} />
-                </Grid>
-            </Grid>
-
-            <br/>
-            
+            </Grid>    
+                    
             <Grid item lg={6} md={12}>
-                <Grid>
-                    <Typography variant='h4'>
-                        Ordered By Administrative Officer
-                    </Typography>
-                </Grid>
-                <Grid>
-                    <MaterialForm type = {'ordered'} submitHandler = {orderedHandler } />
-                </Grid>
-                <Grid>
-                    <MaterialTable data = {orderedMaterial} />
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <Typography variant='h4'>
+                            Ordered By Administrative Officer
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <MaterialForm type = {'ordered'} submitHandler = {orderedHandler } />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <MaterialTable data = {orderedMaterial} setData = {setOrderedMaterial} />
+                    </Grid>
                 </Grid>
             </Grid>
         </Grid>
--- client/src/forms/components/MaterialTable.js
+++ client/src/forms/components/MaterialTable.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import { 
+    TableContainer, 
+    Paper, 
+    Table, 
+    TableHead, 
+    TableRow, 
+    TableCell, 
+    TableBody,
+    IconButton
+} from '@material-ui/core';
+import { Delete, Edit } from '@material-ui/icons';
 
-export default function MaterialTable({ data }) {
+export default function MaterialTable({ data, setData }) {
+    const deleteHandler = (index) => {
+        setData(data.filter((item,i) => {
+            return i !== index;
+        }))
+    }
+    
     return(
-        <>
-            {JSON.stringify(data)}
-        </>
+        <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Material</TableCell>
+                        <TableCell align="right">Approx Cost</TableCell>
+                        <TableCell align="right">Edit</TableCell>
+                        <TableCell align="center">Delete</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {data.map((item,index) => (
+                        <TableRow key={index}>
+                            <TableCell component="th" scope="row">{item.material}</TableCell>
+                            <TableCell component="th" scope="row" align="right">{item.approxCost}</TableCell>
+                            <TableCell align="right">
+                                <IconButton size='small'>
+                                    <Edit fontSize='small' />
+                                </IconButton>
+                            </TableCell>
+                            <TableCell align="center">
+                                <IconButton size='small' onClick={() => deleteHandler(index)}>
+                                    <Delete fontSize='small' />
+                                </IconButton>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
