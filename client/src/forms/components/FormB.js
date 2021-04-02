2eed9fe8909578497e6dec4341a428989eac5d10
kakarot_6909 <rahul.naravadkar@gmail.com>
2021-04-02 19:17:04 +0530

    Refactor. Removes unwanted imports from FormB component

--- client/src/forms/components/FormB.js
+++ client/src/forms/components/FormB.js
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from 'react';
+import React,{ useState} from 'react';
 import { Grid, Typography } from '@material-ui/core';
 
 import MaterialForm from './MaterialForm';
