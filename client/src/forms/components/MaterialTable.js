bc82d6c139b8386d067236dc73a9a47892b2ff30
kakarot_6909 <rahul.naravadkar@gmail.com>
2021-04-03 16:03:21 +0530

    Fix. Fixes most of the things in MaterialTable

--- client/src/forms/components/MaterialTable.js
+++ client/src/forms/components/MaterialTable.js
@@ -9,13 +9,14 @@ import {
     TableBody,
     IconButton,
     FormControl,
-    TextField
+    TextField,
 } from '@material-ui/core';
 import { DeleteOutline, DoneOutline, Edit } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/core/styles'
 
 import Confirmation from '../../helpers/components/Confirmation';
 import PopOver from '../../helpers/components/PopOver';
+import MaterialFormValidator from '../utils/MaterialFormValidator';
 
 const useStyles = makeStyles(() => ({
     costInput: {
@@ -24,6 +25,9 @@ const useStyles = makeStyles(() => ({
           margin: 0
         }
     },
+    inputLabel: {
+        textAlign: 'right',
+    }
 }))
 
 export default function MaterialTable({ data, setData }) {
@@ -34,10 +38,11 @@ export default function MaterialTable({ data, setData }) {
     const [popoverEvent, setPopoverEvent] = useState(null);
     const [popoverVisible, setPopoverVisible] = useState(false);
     const [delIndex, setDelIndex] = useState(-1);
+    const [errors, setErrors] = useState({});
     const [inEditMode, setInEditMode] = useState({
         state: false,
         row: null,
-    })
+    });
 
     const resetPopoverStates = () => {
         setPopoverEvent(null);
@@ -51,12 +56,33 @@ export default function MaterialTable({ data, setData }) {
         setPopoverVisible(true);
     };
 
+    const isMaterialExists = (array, value) => array.findIndex((item) => item.material === value) >= 1;
+
+    const checkDuplicate = (material) => {
+        if (!isMaterialExists(data, material)){
+            setErrors({});
+            return Promise.resolve({
+                status: true,
+                errors: errors
+            });
+        } else{
+            const error = {
+                material: ['Material already Exists']
+            }
+            setErrors(error);
+            return Promise.reject({
+                status: false,
+                errors: error
+            })
+        }
+    };
+
     const deleteHandler = () => {
         setData(data.filter((item,i) => {
             return i !== delIndex;
         }));
         resetPopoverStates();
-    }
+    };
 
     const editHandler = (index,item) => {
         setMaterial(item.material);
@@ -65,14 +91,32 @@ export default function MaterialTable({ data, setData }) {
             state: true,
             row: index
         })
-    }
+    };
 
     const saveHandler = (index) => {
-        setInEditMode({
-            state: false,
-            row: null
-        })
-    }
+        MaterialFormValidator()
+        .validate({
+            material,
+            approxCost,
+        }).then(() => {
+            checkDuplicate(material).then(() => {
+                setInEditMode({
+                    state: false,
+                    row: null
+                });
+
+                const editedData = [...data];
+                editedData[index] = {material,approxCost};
+                setData(editedData);
+            }, error => {
+                console.log(error)
+                setErrors(error.errors);
+            })
+        }, (error => {
+            console.log(error)
+            setErrors(error.errors);
+        }))
+    };
 
     const popoverContent = (
         <Confirmation confirmText={'Are you sure?'} onResolve={deleteHandler} onReject={resetPopoverStates} />
@@ -81,7 +125,7 @@ export default function MaterialTable({ data, setData }) {
     const editModeForm = (index,item) => {
         return (
             <TableRow key={index}>
-                <TableCell component="th" scope="row">
+                <TableCell component="th" scope="row" width='50%'>
                     <FormControl>
                         <TextField
                             fullWidth
@@ -92,57 +136,62 @@ export default function MaterialTable({ data, setData }) {
                             size="small"
                             value={material}
                             onChange={(event) => setMaterial(event.target.value)}
-                            // error={!!errors.material}
-                            // helperText={errors.material ? errors.material[0] : ' '}
+                            error={!!errors.material}
+                            helperText={errors.material ? errors.material[0] : ' '}
                         />
                     </FormControl>
                 </TableCell>
-                <TableCell component="th" scope="row" align="right">
+                <TableCell component="th" scope="row" align="right" width='30%'>
                     <FormControl>
                         <TextField
                             className={classes.costInput}
+                            inputProps={{
+                                shrink: true,
+                                'data-testid': 'cost', 
+                                style: { textAlign: 'right' }
+                            }}
+                            InputLabelProps={{
+                                shrink: true,
+                                classes: classes.inputLabel
+                            }}
                             type='number'
                             fullWidth
                             required
                             autoFocus
-                            inputProps={{ 'data-testid': 'cost' }}
                             label="Cost"
                             size="small"
                             value={approxCost}
                             onChange={(event) => setApproxCost(event.target.value)}
-                            // error={!!errors.approxCost}
-                            // helperText={errors.approxCost ? errors.approxCost[0] : ' '}
+                            error={!!errors.approxCost}
+                            helperText={errors.approxCost ? errors.approxCost[0] : ' '}
                         />
                     </FormControl>    
                 </TableCell>
-                <TableCell align="right">
-                    <IconButton size='small' onClick={() => saveHandler(index)}>
+                <TableCell component="th" scope="row" align="center" width='20%'>
+                    <IconButton onClick={() => saveHandler(index)}>
                         <DoneOutline style={{ color: 'black' }} fontSize='small' />
-                    </IconButton>
-                    <IconButton disabled size='small' onClick={(event) => showPopover(event,index)}>
-                        <DeleteOutline style={{ color: 'red' }} fontSize='small' />
                     </IconButton>
                 </TableCell>
             </TableRow>
         )
-    }
+    };
 
     function displayInfo(index,item){
         return (
             <TableRow key={index}>
-                <TableCell component="th" scope="row">{item.material}</TableCell>
-                <TableCell component="th" scope="row" align="right">{item.approxCost}</TableCell>
-                <TableCell align="right">
-                    <IconButton size='small' disabled={inEditMode.state} onClick={(event) => editHandler(index,item)}>
+                <TableCell component="th" scope="row" width='50%'>{item.material}</TableCell>
+                <TableCell component="th" scope="row" align="right" widht='30%'>{item.approxCost}</TableCell>
+                <TableCell component="th" scope="row" width='20%' align="center">
+                    <IconButton style={{padding: '10px'}} size='small' disabled={inEditMode.state} onClick={(event) => editHandler(index,item)}>
                         <Edit style={{ color: 'black' }} fontSize='small' />
                     </IconButton>
-                    <IconButton size='small' onClick={(event) => showPopover(event,index)}>
+                    <IconButton style={{padding: '10px'}} size='small' onClick={(event) => showPopover(event,index)}>
                         <DeleteOutline style={{ color: 'red' }} fontSize='small' />
                     </IconButton>
                 </TableCell>
             </TableRow>
         )
-    }
+    };
     
     return(
         <TableContainer component={Paper}>
@@ -150,11 +199,11 @@ export default function MaterialTable({ data, setData }) {
             <Table aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                        <TableCell>Material</TableCell>
-                        <TableCell align={inEditMode.state ? 'left' : 'right'}>
+                        <TableCell width='50%'>Material</TableCell>
+                        <TableCell width='30%' align={inEditMode.state ? 'left' : 'right'}>
                             Approx Cost
                         </TableCell>
-                        <TableCell align="right">Actions</TableCell>
+                        <TableCell width='20%' align="center">Actions</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
