b762945bbb282f0d76d2729ac154a97ced8062b7
kakarot_6909 <rahul.naravadkar@gmail.com>
2021-04-08 13:15:52 +0530

    Refactor. Adds a to do type for room/location in complaint form

--- client/src/forms/components/UserComplaintForm.js
+++ client/src/forms/components/UserComplaintForm.js
@@ -9,9 +9,11 @@ import {
     FormGroup,
     FormControlLabel,
     Radio,
+    Chip,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import UserComplaintFormValidator from '../utils/UserComplaintFormValidator';
+import { DeleteOutline } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
     button: {
@@ -23,7 +25,11 @@ const useStyles = makeStyles((theme) => ({
     },
     formControl: {
         width: '100%',
-    }
+    },
+    chip: {
+        margin: theme.spacing(0.5),
+        maxWidth: '100%'
+    },
 }));
 
 export default function UserComplaintForm() {
@@ -31,35 +37,80 @@ export default function UserComplaintForm() {
 
     const [department, setDepartment] = useState('');
     const [location, setLocation] = useState('');
+    const [locations, setLocations] = useState([]);
     const [workType, setWorkType] = useState('');
     const [workDetails, setWorkDetails] = useState('');
     const [otherWork, setOtherWork] = useState('');
     const [errors, setErrors] = useState({});
 
     const resetForm = () => {
-        // setDepartment('');
-        // setLocation('');
+        setDepartment('');
+        setLocation('');
+        setWorkType('');
+        setWorkDetails('');
+        setOtherWork('');
+        setLocations([]);
         setErrors({});
     };
 
     const handleChange = (event) => {
         setWorkType(event.target.value);
+    };
+
+    const addLocationHandler = (event) => {
+        event.preventDefault();
+
+        if(location === ''){
+            setErrors({
+                ...errors,
+                locations: ['Enter a Location'],
+            });
+            return;
+        }
+        setLocations([
+            ...locations ,
+            location,
+        ])
+        setLocation('');
+        setErrors({
+            ...errors,
+            locations: null,
+        })
+    };
+
+    const removeLocationHandler = (toBeRemoved) => {
+        setLocations(locations.filter(location => toBeRemoved!==location));
     }
 
     const submitHandler = (event) => {
         event.preventDefault();
 
+        if(location !== ''){
+            setErrors({
+                ...errors,
+                locations: ['Add or Empty the Location to Proceed']
+            })
+            return;
+        }
+
         try{
             UserComplaintFormValidator()
             .validate({
                 department,
-                location,
+                locations,
                 workType,
                 workDetails
             })
-            .then(
-                () => resetForm(),
-                (error) => setErrors(error.errors)
+            .then(() => {
+                if(workType === 'Other' && otherWork === ''){
+                    setErrors({
+                        ...errors,
+                        otherWork: ['Enter Work Type']
+                    })
+                    return;
+                }
+                resetForm();
+            }, (error) => setErrors(error.errors)
             );
         }catch(error){
             setErrors(error.errors);
@@ -88,23 +139,54 @@ export default function UserComplaintForm() {
                             />
                         </FormControl>
                     </Grid>
+                    <form>
+                        <Grid container spacing={2}>
+                            <Grid item md={8} xs={8}>
+                                <FormControl className={classes.formControl}>
+                                    <TextField
+                                        className={classes.style}
+                                        fullWidth
+                                        required
+                                        autoFocus
+                                        inputProps={{ 'data-testid': 'location' }}
+                                        label="Room/Location"
+                                        variant="outlined"
+                                        size="small"
+                                        value={location}
+                                        onChange={(event) => setLocation(event.target.value)}
+                                        error={!!errors.locations}
+                                        helperText={errors.locations ? errors.locations[0] : ' '}
+                                    />
+                                </FormControl>
+                            </Grid>
+                            <Grid item md={4} xs={4}>
+                                <Button
+                                    className={[classes.button, classes.style].join(' ')}
+                                    fullWidth
+                                    type="submit"
+                                    color='primary'
+                                    variant="contained"
+                                    onClick={addLocationHandler}
+                                >
+                                    Add
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </form>
                     <Grid>
-                        <FormControl className={classes.formControl}>
-                            <TextField
-                                className={classes.style}
-                                fullWidth
-                                required
-                                autoFocus
-                                inputProps={{ 'data-testid': 'location' }}
-                                label="Room/Location"
-                                variant="outlined"
-                                size="small"
-                                value={location}
-                                onChange={(event) => setLocation(event.target.value)}
-                                error={!!errors.location}
-                                helperText={errors.location ? errors.location[0] : ' '}
-                            />
-                        </FormControl>
+                        {locations.map((location, index) => {
+                            return (
+                                <Chip 
+                                    key={index} 
+                                    className={classes.chip}
+                                    label={location}
+                                    color='primary'
+                                    variant='outlined'
+                                    deleteIcon={<DeleteOutline style={{color: 'red'}} />}
+                                    onDelete={() => removeLocationHandler(location)}
+                                />
+                            )
+                        })}
                     </Grid>
                 </Grid>
                 <Grid item xs={12} md={8}>
@@ -205,18 +287,11 @@ export default function UserComplaintForm() {
                                                     />
                                                 </FormControl>
                                             </Grid>)
-                                        : (<Grid item xs={6} md={3}></Grid>)
+                                        : (<Grid item xs={6} md={3} style={{marginBottom: '69px'}}></Grid>)
                                     }
                                 </Grid>
                             </FormGroup>
                         </FormControl>
-                        <Typography
-                            variant="subtitle2"
-                            color="error"
-                            data-testid="non-field-errors"
-                        >
-                            {errors['workType'] ? errors['workType'][0] : ' '}
-                        </Typography>
                     </Grid>
                     <FormControl className={classes.formControl}>
                         <TextField
--- client/src/forms/utils/UserComplaintFormValidator.js
+++ client/src/forms/utils/UserComplaintFormValidator.js
@@ -1,12 +1,13 @@
 import FormValidator from '../../helpers/FormValidator';
 
 const emptyCheck = (value) => value && value.trim().length > 0;
+const emptyArrayCheck = (value) => value.length > 0;
 
 export default function UserComplaintValidator() {
     const validator = new FormValidator();
     validator
       .addRule('department', emptyCheck, 'Please Enter Department')
-      .addRule('location', emptyCheck, 'Please Enter Location')
+      .addRule('locations', emptyArrayCheck, 'Enter Atleast One Location')
       .addRule('workType', emptyCheck, 'Please Select Nature of Work')
       .addRule('workDetails', emptyCheck, 'Please Enter Work Details');
 
