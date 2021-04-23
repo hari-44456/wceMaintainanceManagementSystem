e4028a1c78ef4e300ef418400fa0c918cbc5ba91
kakarot_6909 <rahul.naravadkar@gmail.com>
2021-04-23 11:40:47 +0530

    Refactor. Makes use of Grid for Util

--- client/src/Dashboards/Util.js
+++ client/src/Dashboards/Util.js
@@ -7,32 +7,23 @@ import {
   Select,
   Button,
   Box,
+  Grid
 } from '@material-ui/core';
 import ReceiptIcon from '@material-ui/icons/Receipt';
 import AddIcon from '@material-ui/icons/Add';
 import { makeStyles } from '@material-ui/core/styles';
-
 import SearchBar from 'material-ui-search-bar';
 
 import './style.css';
 
 const useStyles = makeStyles((theme) => ({
-  table: {
-    minWidth: 650,
+  formControl: {
+    width: '100%',
   },
-  selectDropdown: { color: '#fff', backgroundColor: '#1b1f38' },
-  menuItem: {
-    '&:hover': {
-      backgroundColor: '#3b3f58',
-    },
-    formControl: {
-      margin: theme.spacing(1),
-      minWidth: 120,
-    },
-    selectEmpty: {
-      marginTop: theme.spacing(2),
-    },
-  },
+  searchBar: {
+    width: '100%',
+    height: '100%'
+  }
 }));
 
 export default function Util({
@@ -47,82 +38,66 @@ export default function Util({
 }) {
   const classes = useStyles();
   return (
-    <>
-      <div className="Title">
+    <Grid container spacing={3}>
+      <Grid item xs={12} className="Title">
         <ReceiptIcon />
         <h1>User Complaints</h1>
-      </div>
-      <div style={{ width: '150%' }}>
-        <Box
-          display="flex"
-          flexDirection="row"
-          p={1}
-          m={1}
-          bgcolor="background.paper"
-        >
-          <Box p={1} bgcolor="grey.300">
+      </Grid>
+      <Grid item xs={12} style={{backgroundColor: 'lightgrey'}}>
+        <Grid container spacing={2}>
+          <Grid item md={3} xs={6}>
             <SearchBar
+              className={classes.searchBar}
               value={searched}
-              style={{ width: '100%' }}
               onChange={(searchVal) => setSearched(searchVal)}
               onCancelSearch={() => cancelSearch()}
             />
-          </Box>
-          <Box p={1} bgcolor="grey.300">
-            <div>
-              <FormControl
-                style={{ minWidth: 120 }}
-                variant="outlined"
-                className={classes.formControl}
+          </Grid>
+          <Grid item md={3} xs={6}>
+            <FormControl
+              variant="outlined"
+              className={classes.formControl}
+            >
+              <InputLabel>
+                Sort By
+              </InputLabel>
+              <Select
+                value={sort}
+                onChange={handleSortDrop}
+                label="Sort By"
               >
-                <InputLabel id="demo-simple-select-outlined-label">
-                  Sort By
-                </InputLabel>
-                <Select
-                  labelId="demo-simple-select-outlined-label"
-                  id="demo-simple-select-outlined"
-                  value={sort}
-                  onChange={handleSortDrop}
-                  label="Sort By"
-                >
-                  <MenuItem value="">
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value={'id'}>id</MenuItem>
-                  <MenuItem value={'date'}>Date</MenuItem>
-                  <MenuItem value={'title'}>Title</MenuItem>
-                  <MenuItem value={'status'}>Status</MenuItem>
-                  <MenuItem value={'department'}>Departement</MenuItem>
-                </Select>
-              </FormControl>
-            </div>
-          </Box>
-          <Box p={1} bgcolor="grey.300">
-            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-              <FormControl
-                style={{ minWidth: 120 }}
-                variant="outlined"
-                className={classes.formControl}
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={'id'}>id</MenuItem>
+                <MenuItem value={'date'}>Date</MenuItem>
+                <MenuItem value={'title'}>Title</MenuItem>
+                <MenuItem value={'status'}>Status</MenuItem>
+                <MenuItem value={'department'}>Department</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item md={3} xs={6}>
+            <FormControl
+              variant="outlined"
+              className={classes.formControl}
+            >
+              <InputLabel>
+                Filter By
+              </InputLabel>
+              <Select
+                value={filter}
+                onChange={handleFilter}
+                label="Filter By"
               >
-                <InputLabel id="demo-simple-select-outlined-label">
-                  Filter By
-                </InputLabel>
-                <Select
-                  labelId="demo-simple-select-outlined-label"
-                  id="demo-simple-select-outlined"
-                  value={filter}
-                  onChange={handleFilter}
-                  label="Filter By"
-                >
-                  <MenuItem value="">
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value={'status'}>Status</MenuItem>
-                </Select>
-              </FormControl>
-            </div>
-          </Box>
-          <Box p={1} bgcolor="grey.300">
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={'status'}>Status</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item md={3} xs={6}>
             {match.url.substring(match.url.lastIndexOf('/')) === 'student' && (
               <Link to="/ui/forms/complaint">
                 <Button size="large" variant="contained" color="primary">
@@ -131,10 +106,9 @@ export default function Util({
                 </Button>
               </Link>
             )}
-          </Box>
-        </Box>
-      </div>
-      <br></br>
-    </>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
