108fc167062cfc29ad735ae946bfe39506195152
hari-44456 <naraharipapshetwar@gmail.com>
2021-04-30 21:33:30 +0700

    Add. added auth middleware to user complaint form

--- client/src/forms/pages/Complaint.js
+++ client/src/forms/pages/Complaint.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Typography } from '@material-ui/core';
 
 import UserComplaintForm from '../components/UserComplaintForm';
+import axiosInstance from '../../helpers/axiosInstance';
 
-export default function Complaint(){
-    return(
-        <React.Fragment>
-            <Typography variant="h4">
-                User Complaint Form
-            </Typography>
-            <UserComplaintForm />
-        </React.Fragment>
-    )
+export default function Complaint() {
+  const history = useHistory();
+
+  useEffect(() => {
+    const isAuthenticated = async () => {
+      try {
+        const result = await axiosInstance.get('/api/isAuthenticated');
+        if (!result.data.success) history.push('/ui/login/student');
+      } catch (error) {
+        history.push('/ui/login/student');
+      }
+    };
+    isAuthenticated();
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Typography variant="h4">User Complaint Form</Typography>
+      <UserComplaintForm />
+    </React.Fragment>
+  );
 }
