940495a132a693bbb29c4539239892ee186ede9f
hari-44456 <naraharipapshetwar@gmail.com>
2021-05-28 13:25:02 +0700

    Refactor. refactored 404 page

--- client/src/helpers/components/PageNotFound404.js
+++ client/src/helpers/components/PageNotFound404.js
@@ -3,64 +3,46 @@ import React from 'react';
 import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
 import { Link } from 'react-router-dom';
 import SearchIcon from '@material-ui/icons/Search';
-import { Card, Divider, Grid, Breadcrumbs, Button } from '@material-ui/core';
+import { Divider, Grid, Button, Typography } from '@material-ui/core';
 
 export default function PageNotFound404() {
   return (
-    <>
-      <Breadcrumbs aria-label="breadcrumb">
-        <Link color="inherit" to="/ui">
-          Home
-        </Link>
-        <Link color="inherit" to="/ui/login">
-          Login
-        </Link>
-        <Link color="textPrimary" aria-current="page">
-          404
-        </Link>
-      </Breadcrumbs>
-      <Grid>
-        {/* <Card raised className="py-4 text-center"> */}
-        <Grid container className="m-auto">
-          <Grid item xs={12} md={6}>
-            <h1>
-              <img
-                src="https://via.placeholder.com/150"
-                alt="Maintanance Management System 404"
-                height="50px"
-                className="mr-3"
-              />
-              <b>Maintanance Management System</b>
-            </h1>
-            <h6 className="text-muted">Page not found ...</h6>
-            <h5 className="my-4">
-              We're sorry, but it looks like you are lost&nbsp;
-              <SearchIcon />
-              <br />
-              <br /> The Web address you entered is not a functioning page on
-              our site.
-            </h5>
-            <Link to="/ui">
-              <Button variant="success" size="lg">
-                <HomeRoundedIcon />
-                &nbsp;
-                <strong>GO TO HOME</strong>
-              </Button>
-            </Link>
-          </Grid>
-          <Grid item md={1}>
-            <Divider orientation="vertical" />
-          </Grid>
-          <Grid md={5} className="m-auto">
-            <img
-              src="https://blog.thomasnet.com/hs-fs/hubfs/shutterstock_774749455.jpg?width=600&name=shutterstock_774749455.jpg"
-              alt="404"
-              fluid
-            />
+    <Grid container>
+      <Grid container>
+        <Grid item md={6} xs={12}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} align="center">
+              <Typography variant="h4">
+                <b>Page not found ...</b>
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h6">
+                We're sorry, but it looks like you are lost&nbsp;
+                <SearchIcon />
+                <br />
+                <br /> The Web address you entered is not a functioning page on
+                our site.
+              </Typography>
+            </Grid>
+            <Grid xs={12} align="center">
+              <Link to="/ui" style={{ textDecoration: 'none' }}>
+                <Button variant="contained" size="large" color="secondary">
+                  <HomeRoundedIcon />
+                  &nbsp;
+                  <strong>GO TO HOME</strong>
+                </Button>
+              </Link>
+            </Grid>
           </Grid>
         </Grid>
-        {/* </Card> */}
+        <Grid item md={1}>
+          <Divider orientation="vertical" />
+        </Grid>
+        <Grid item md={5} xs={12} align="center">
+          <img width="100%" src="/404.webp" alt="404" />
+        </Grid>
       </Grid>
-    </>
+    </Grid>
   );
 }
