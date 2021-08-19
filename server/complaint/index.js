e047cf480455d081c1270a8f1a446b22c0507893
hari-44456 <naraharipapshetwar@gmail.com>
2021-08-19 23:42:11 +0700

    Refactor. refactored bug while rejecting complaint by admin when material is not added

--- server/complaint/index.js
+++ server/complaint/index.js
@@ -11,17 +11,21 @@ const User = require('../login/model');
 // const sendMail = require('../mail');
 const Material = require('../material/model');
 const Store = require('../store/model');
-const { generatePdf, removePdf } = require('../pdf');
 const { userRole, validUserRoles } = require('../utils/userRole');
 
 const delteMaterialAndUpdateStore = (complaintId) => {
   return new Promise(async (resolve, reject) => {
     try {
-      const { availableInStore } = await Material.findOne(
+      const data = await Material.findOne(
         { complaintId },
         { availableInStore: 1 }
       );
 
+      if (!data || !data.availableInStore) {
+        resolve({ success: 1 });
+        return;
+      }
+      const availableInStore = data.availableInStore;
       availableInStore.forEach(
         async (material) =>
           await Store.updateOne(
@@ -244,9 +248,7 @@ router.post('/accept/:id', verify, validateAcceptSchema, async (req, res) => {
         }
       );
 
-      // await generatePdf();
       // await sendMail(email, status, true);
-      // await removePdf();
     }
 
     if (req.user.userType === 'committee') {
